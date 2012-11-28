872540692f4c567584a4069f931c0ab0a2922410
pc <paclough@github>
2012-11-27 22:05:34 -0600

    Add SessionsHelper to ApplicationController. Add create method to SessionsController. Add create_remember_token method to User model. Add fields to signin page using form_for. Add remember_token to User model. Add additional tests for signin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  before_save { self.email.downcase! }
+  before_save { | user | user.email = email.downcase }
+  before_save :create_remember_token
 
   validates( :name, presence: true, length: { maximum: 50 } )
 
@@ -27,4 +28,9 @@ class User < ActiveRecord::Base
 
   validates( :password_confirmation, presence: true )
 
+  private
+
+    def create_remember_token
+        self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
