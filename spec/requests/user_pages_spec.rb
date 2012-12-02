c7ff338d48425ac2a0be4ccfc2d5ae7e94bf1dba
pc <paclough@github>
2012-12-02 01:11:32 -0600

    Add bootstrap to javascript assets for dropdown menu. Add destroy method to Session controller. Add sign_in function during initial user signup process. Add sign_in, sign_out, etc. as helper methods to Sessions. Add dropdown menu and conditional links (sign in/out) to _header. Add authentication test for successful signout. Add UserPages tests for successful signout.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,6 +53,16 @@ describe "UserPages" do
 				expect { click_button submit }.to change( User, :count ).by( 1 )
 
 			end
+
+			describe "after saving the user" do
+				# Listing 7.32
+				before { click_button submit }
+				let( :user ) { User.find_by_email( 'user@example.com' ) }
+
+				it { should have_selector( 'title', text: user.name ) }
+				it { should have_selector( 'div.alert.alert-success', text: 'Welcome' ) }
+				it { should have_link( 'Sign out' ) }
+			end
 		end
 	end
 
